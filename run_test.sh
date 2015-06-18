21a53acea842674046bf5aa5f096a6c1c708b855
Lee <jessy1092@gmail.com>
2015-06-18 10:50:08 +0800

    Remove exception.py because cinder already merge driver.

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,6 @@ export CINDER_DIR=./cinder
 export CINDER_REPO_URL=https://git.openstack.org/openstack/cinder
 export CINDER_TEST_DIR=cinder/tests/unit
 export INFORTREND_DRIVER_DIR=cinder/volume/drivers/infortrend
-export CINDER_LIB=./cinder/cinder
 
 if [ -d "$CINDER_DIR" ]; then
     rm -rf $CINDER_DIR
@@ -18,7 +17,6 @@ fi
 
 cp ./src/* $CINDER_DIR/$INFORTREND_DRIVER_DIR/ -r
 cp ./test/* $CINDER_DIR/$CINDER_TEST_DIR/ -r
-cp ./lib/* $CINDER_LIB/ -r
 
 cd $CINDER_DIR
 
