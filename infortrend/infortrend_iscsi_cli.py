5885ceebd8b34cb684bd946c1ffd3f079c99aafc
johnnychou <johnny800503.work@gmail.com>
2016-11-07 15:20:24 +0800

    v1.0.3 - Add iSCSI MPIO support

--- infortrend/infortrend_iscsi_cli.py
+++ infortrend/infortrend_iscsi_cli.py
@@ -32,6 +32,7 @@ class InfortrendCLIISCSIDriver(driver.ISCSIDriver):
         1.0.0 - Initial driver
         1.0.1 - Support DS4000
         1.0.2 - Support GS Series
+        1.0.3 - Add iSCSI MPIO support
     """
 
     def __init__(self, *args, **kwargs):
